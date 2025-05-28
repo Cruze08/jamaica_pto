b5f842d502411149df36db6666d318e88d26fe08
Cruze08 <ankit.m0720@gmail.com>
2025-05-28 21:33:29 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Jamaica Pto
+
+Hy
+
+#### License
+
+MIT
---
+++ jamaica_pto/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Jamaica Pto",
+			"type": "module",
+			"label": _("Jamaica Pto")
+		}
+	]
